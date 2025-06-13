f6f62ad7d5abce12a161e423cd9c04e9345c799e
Rebecca Claydon <rebecca.claydon@hotmail.com>
2025-06-13 11:12:09 +0200

    almost

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,11 @@
 class ReviewsController < ApplicationController
+  # before_action :set_restaurant, only: %i[new create]
+
+  # def new
+  #   @restaurant = Restaurant.find(params[:restaurant_id])
+  #   @review = Review.new
+  # end
+
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
@@ -11,6 +18,10 @@ class ReviewsController < ApplicationController
   end
 
   private
+
+  # def set_restaurant
+  #   @restaurant = Restaurant.find(params[:restaurant_id])
+  # end
 
   def review_params
     params.require(:review).permit(:rating, :content)
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ApplicationRecord
   belongs_to :restaurant
   validates :content, :rating, presence: true
   validates :rating, numericality: { only_integer: true }
-  # , inclusion: { in: %w(1 2 3 4 5)}
+  validates :rating, inclusion: { in: 0..5 }
 end
